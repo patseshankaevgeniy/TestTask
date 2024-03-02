59e045f110287a7b8fbf4244e667a0a449b3a78e
Patseshanka Eugeniy <patseshanka.eugeniy@gmail.com>
2024-03-02 16:00:56 +0300

    add logic for services

--- TestTask/Migrations/20240302122930_UpdateDb.Designer.cs
+++ TestTask/Migrations/20240302122930_UpdateDb.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestTask.Data;
 
@@ -10,9 +11,11 @@ using TestTask.Data;
 namespace TestTask.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240302122930_UpdateDb")]
+    partial class UpdateDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
